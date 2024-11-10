88e2981de9556a5af02e2cb1419b9aa48bbeeb79
Prasanth Sathiya <s.prasanth2611@gmail.com>
2024-11-10 14:44:25 +0400

    minor changes

--- Search_and_sort_pipeline.py
+++ Search_and_sort_pipeline.py
@@ -1,10 +1,40 @@
 import array
 import random
 def generate_sorted_data(size):
-    array_a=array.array('i',[0]*size)
-    for i in range(len(array_a)):
-        array_a[i]=random.randint(1,100)
+    arr=array.array('i',[0]*size)
+    for i in range(len(arr)):
+        arr[i]=random.randint(1,100)
     if size>999:
+        if len(arr)>1:
+            left_arr=arr[:len(arr)//2]
+            right_arr=arr[len(arr)//2:]
+
+            merge_sort(left_arr)
+            merge_sort(right_arr)
+
+            i=0
+            j=0
+            k=0
+
+            while i<len(left_arr) and j<len(right_arr):
+                if left_arr[i]<right_arr[j]:
+                    arr[k]=left_arr[i]
+                    i+=1
+                else:
+                    arr[k]=right_arr[j]
+                    j+=1
+                k+=1
+            
+            while i<len(left_arr):
+                arr[k]=left_arr[i]
+                i+=1
+                k+=1
+            while j<len(right_arr):
+                arr[k]=right_arr[j]
+                j+=1
+                k+=1
+    
+
         
 
     return array_a
