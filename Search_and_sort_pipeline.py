29339bf15ea663de27010d713c0ee9ec2c2620b2
Prasanth Sathiya <s.prasanth2611@gmail.com>
2024-11-10 18:01:32 +0400

    testing

--- Search_and_sort_pipeline.py
+++ Search_and_sort_pipeline.py
@@ -5,39 +5,40 @@ def generate_sorted_data(size):
     for i in range(len(arr)):
         arr[i]=random.randint(1,100)
     if size>999:
-        if len(arr)>1:
-            left_arr=arr[:len(arr)//2]
-            right_arr=arr[len(arr)//2:]
+        def merge_sort(arr):
+            if len(arr)>1:
+                left_arr=arr[:len(arr)//2]
+                right_arr=arr[len(arr)//2:]
 
-            merge_sort(left_arr)
-            merge_sort(right_arr)
+                merge_sort(left_arr)
+                merge_sort(right_arr)
 
-            i=0
-            j=0
-            k=0
+                i=0
+                j=0
+                k=0
 
-            while i<len(left_arr) and j<len(right_arr):
-                if left_arr[i]<right_arr[j]:
+                while i<len(left_arr) and j<len(right_arr):
+                    if left_arr[i]<right_arr[j]:
+                        arr[k]=left_arr[i]
+                        i+=1
+                    else:
+                        arr[k]=right_arr[j]
+                        j+=1
+                    k+=1
+                
+                while i<len(left_arr):
                     arr[k]=left_arr[i]
                     i+=1
-                else:
+                    k+=1
+                while j<len(right_arr):
                     arr[k]=right_arr[j]
                     j+=1
-                k+=1
-            
-            while i<len(left_arr):
-                arr[k]=left_arr[i]
-                i+=1
-                k+=1
-            while j<len(right_arr):
-                arr[k]=right_arr[j]
-                j+=1
-                k+=1
+                    k+=1
     
 
         
 
-    return array_a
+    return arr
 
 
 print(generate_sorted_data(5))
